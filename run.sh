d923d2b8f81d8639b18b99ee16e8ce1d36c13d30
Jos De Roo <josderoo@gmail.com>
2023-09-12 19:58:30 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@ echo ""
 echo "Filter packages on packagedBy Ruben"
 echo ""
 
-eye --quiet --nope --blogic data/* logic/filter-package/* > intermediate/run1.n3
+eye --quiet --nope data/* logic/filter-package/* > intermediate/run1.n3
 
 cat intermediate/run1.n3
 
@@ -10,7 +10,7 @@ echo ""
 echo "Flatten resulting data"
 echo ""
 
-eye --quiet --nope --blogic --no-qnames intermediate/run1.n3 logic/flatten/* > intermediate/result.n3
+eye --quiet --nope --no-qnames intermediate/run1.n3 logic/flatten/* > intermediate/result.n3
 
 cat intermediate/result.n3
 
